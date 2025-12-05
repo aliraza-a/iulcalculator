a4e0f509e08d3f434d44f2bf4ed72eb1eccaed80
aliraza-a <razaa8075@gmail.com>
2025-12-06 03:28:43 +0500

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   output: "standalone",
-//   experimental: {
-//     serverActions: {
-//       allowedOrigins: ["iul-calculator-pro-nu.vercel.app/", "localhost:3000"], // Add your production domain
-//     },
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true, // Skips ESLint during Vercel build
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "x1pmtwto3h70vgs8.public.blob.vercel-storage.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
@@ -32,7 +5,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
     serverActions: {
-      allowedOrigins: ["iul-calculator-pro-nu.vercel.app", "localhost:3000"],
+      allowedOrigins: ["iulcalculator.vercel.app", "localhost:3000"],
     },
   },
   eslint: {
@@ -42,9 +15,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "x1pmtwto3h70vgs8.public.blob.vercel-storage.com",
-        port: "",
-        pathname: "/**",
+        hostname: "*.public.blob.vercel-storage.com",
       },
     ],
   },
